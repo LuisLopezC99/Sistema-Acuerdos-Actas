b2d01770a8d24dd435017b485084e9c755e979b8
eduardoojeda <peleojeda@gmail.com>
2023-09-23 16:27:21 -0600

    added findall agreements

--- src/app/api/agreement/route.js
+++ src/app/api/agreement/route.js
@@ -3,16 +3,9 @@ import {prisma} from "../../../libs/prisma"
 
 
 
-const GET = async ()  =>{
-    const insert = await prisma.tab_agreement.create({
-        data: {
-            session_date: '20/20/1996',
-            report: 'Picha',
-            session_type: 'Joder',
-            facebook_link: 'pelos.com'
-        }
-    })
+export const GET = async ()  =>{
     const tab_agreement = await prisma.tab_agreement.findMany()
-    
     return NextResponse.json(tab_agreement)
 }
+
+
