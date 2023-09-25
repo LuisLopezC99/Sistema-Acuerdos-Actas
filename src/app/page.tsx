798be55b974afe843d6644ccf2695e01ae9d4c69
Jose0696 <jose96seque@gmail.com>
2023-09-25 03:43:47 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import styles from './page.module.css'
 import Header from '../app/components/Header'
 import TableAgreement from './components/TableAgre';
 import AddButton from './components/AddButton';
+import SearchButton from './components/SearchButton';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -10,11 +12,13 @@ export default function Home() {
     <div>
       <Header/>
       <AddButton/>
+      <SearchButton/>
       <div>
-      <TableAgreement/>  
-      </div>      
+      <TableAgreement/> 
+      </div>
+         
 
-      
+      <div><Footer/></div>
 
     </div>
   )
