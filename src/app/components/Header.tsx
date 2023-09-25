3c22a7406d291198e3ff2b15f8b00d2b549b3ab4
LuisLopez <lilc1105@gmail.com>
2023-09-24 19:26:45 -0600

    DrawBars

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,74 +1,96 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import Link from "next/link";
 
 const Header = () => {
-    const grayBackground = {
-        backgroundColor: 'rgba(12, 12, 12, 0.12)',
+    const [showEnTramiteMenu, setShowEnTramiteMenu] = useState(false);
+    const [showAcuerdosMenu, setShowAcuerdosMenu] = useState(false);
+
+    const toggleEnTramiteMenu = () => {
+        setShowEnTramiteMenu(!showEnTramiteMenu);
     };
 
+    const toggleAcuerdosMenu = () => {
+        setShowAcuerdosMenu(!showAcuerdosMenu);
+    };
+
+    const hideAcuerdosMenu = () => {
+        setShowAcuerdosMenu(false);
+    };
+
+    const acuerdosItems = [
+        { label: "Redirigido", href: "/acuerdos/redirigido" },
+        { label: "Vencidos", href: "/acuerdos/vencidos" },
+        { label: "Cumplidos", href: "/acuerdos/cumplidos" },
+    ];
+
     return (
-        <Navbar style={grayBackground} variant="dark" expand="lg">
+        <Navbar className="gray-background" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand style={{ marginLeft: '-50px' }}>
+                <Navbar.Brand>
                     <img width={100} height={100} src="icons/logo.png" alt="Logo" />
-                    <p className="texto1 mb-0" style={{ color: 'rgba(3, 12, 4, 0.51)', marginLeft: '-30px' }}>Municipalidad de Tibás</p>
+                    <p className="navbar-brand-text">Municipalidad de Tibás</p>
                 </Navbar.Brand>
 
                 <Nav className="mx-auto flex-column align-items-center">
                     <div className="d-flex flex-column align-items-center mt-auto">
-                        <p style={{ color: '#34A853', fontSize: '35px', marginBottom: '10px' }}>Sistema de Acuerdos y Actas Municipales</p>
+                        <p className="header-text">Sistema de Acuerdos y Actas Municipales</p>
                     </div>
                     <div className="d-flex justify-content-center">
-                        <div className="mx-2" style={{ cursor: 'pointer', marginTop: '37px' }}>
-                            <span onClick={() => window.location.href = '/sesiones'}>
+                        <div className="mx-2">
+                            <Link href="/sesiones">
                                 <span className="nav-link-text">Sesiones</span>
-                            </span>
+                            </Link>
                         </div>
-
-                        <NavDropdown
-                            title={<span className="acuerdos-title">Acuerdos</span>}
-                            id="acuerdos-dropdown"
+                        <div
                             className="mx-2"
-                            style={{ cursor: 'pointer', marginTop: '20px' }}
+                            onMouseEnter={toggleAcuerdosMenu}
+                            onMouseLeave={hideAcuerdosMenu}
                         >
-                            <div className="custom-dropdown-menu" style={{ backgroundColor: '#E5D22D' }}>
-                                <NavDropdown.Item href="/acuerdos/nuevos">
-                                    <span className="nav-link-text">Nuevos</span>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/acuerdos/en-tramite">
+                            <NavDropdown
+                                title={<span className="nav-link-text">Acuerdos</span>}
+                                id="acuerdos-dropdown"
+                            >
+                                {acuerdosItems.map((item, index) => (
+                                    <NavDropdown.Item href={item.href} key={index}>
+                                        <span className="nav-link-text">{item.label}</span>
+                                    </NavDropdown.Item>
+                                ))}
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item
+                                    onMouseEnter={toggleEnTramiteMenu}
+                                    onMouseLeave={toggleEnTramiteMenu}
+                                >
                                     <span className="nav-link-text">En trámite</span>
+                                    {showEnTramiteMenu && (
+                                        <div className="custom-dropdown-menu">
+                                            <span className="nav-link-text">Nuevos</span>
+                                            <span className="nav-link-text">Por vencer</span>
+                                        </div>
+                                    )}
                                 </NavDropdown.Item>
-                                <NavDropdown.Item href="/acuerdos/redirigido">
-                                    <span className="nav-link-text">Redirigido</span>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/acuerdos/vencidos">
-                                    <span className="nav-link-text">Vencidos</span>
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/acuerdos/cumplidos">
-                                    <span className="nav-link-text">Cumplidos</span>
-                                </NavDropdown.Item>
-                            </div>
-                        </NavDropdown>
-
-                        <div className="mx-2" style={{ cursor: 'pointer', marginTop: '37px' }}>
-                            <span onClick={() => window.location.href = '/actas'}>
+                            </NavDropdown>
+                        </div>
+                        <div className="mx-2">
+                            <Link href="/actas">
                                 <span className="nav-link-text">Actas</span>
-                            </span>
+                            </Link>
                         </div>
                     </div>
                 </Nav>
 
                 <Nav>
-                    <Nav.Link href="/account" style={{ marginRight: '-200px' }}>
-                        <img width={100} height={100} src="/icons/account.png" alt="Perfil" />
-                        <p className="texto1 mb-0" style={{ color: 'rgba(3, 12, 4, 0.51)', marginLeft: '30px' }}>Perfil</p>
-                    </Nav.Link>
+                    <Link href="/account">
+                        <span>
+                            <img width={100} height={100} src="/icons/account.png" alt="Perfil" />
+                            <p className="navbar-brand-text">Perfil</p>
+                        </span>
+                    </Link>
                 </Nav>
             </Container>
         </Navbar>
-
     );
 }
+
 export default Header;
