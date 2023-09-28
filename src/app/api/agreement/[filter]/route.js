24827d288eb28981bd31c50cf986852475e2711a
andre <andres.leon.orozco@est.una.ac.cr>
2023-09-28 15:59:17 -0600

    filter agreements enpoint

--- src/app/api/agreement/[filter]/route.js
+++ src/app/api/agreement/[filter]/route.js
@@ -1,33 +1,33 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../libs/prisma"
+import { isNumberObject } from "util/types";
 
 
 export const GET = async (_, { params }) => {
-    const regexDate = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})$/;
-
-    let whereClause = {};
-
-    if (Number(params.filter)) {
-        whereClause.id = Number(params.filter);
-    } else if (params.filter) {
-        whereClause = {
-            OR: [
-                { topic: params.filter },
-                { description: params.filter },
-                { asignedTo: params.filter },
-                { sessionId: Number(params.filter) ? params.filter : -1 },
-                { state: params.filter },
-                { agreementId: params.filter },
-            ],
-        };
-        if (regexDate.test(params.filter)) {
-            whereClause.deadline = params.filter;
-        }
-    }
-
     const agreements = await prisma.tab_agreement.findMany({
-        where: whereClause,
-    });
+        where: {
+            OR: [
+                {
+                    topic: params.filter
+                },
+                {
+                    description: params.filter
+                },
+                {
+                    asignedTo: params.filter
+                },
+                
+                {
+                    state: params.filter
+                },
+                {
+                    agreementId: params.filter
+                },
+            ]
+        },
+        take : 100
+    })
+    return NextResponse.json(agreements)
 }
 
 export default GET
