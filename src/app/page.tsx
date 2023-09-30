ae5af11824b7c2661a363c9987345f0dfb7e29c4
Jennifer <jenniferquiroschacon@gmail.com>
2023-09-30 16:11:32 -0600

    buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.css'
 import Header from '../app/components/Header'
 import TableAgreement from './components/TableAgre';
 import AddButton from './components/AddButton';
+import AddButton2 from './components/AddButton2';
 import SearchButton from './components/SearchButton';
 import Footer from './components/Footer';
 
@@ -12,6 +13,7 @@ export default function Home() {
     <div>
       <Header/>
       <AddButton/>
+      <AddButton2/>
       <SearchButton/>
       <div>
       <TableAgreement/> 
