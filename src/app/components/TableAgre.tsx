34a435f16471c3099545bf4ce4888d4402acad9b
Jose0696 <jose96seque@gmail.com>
2023-10-01 03:14:12 -0600

    mejoras

--- src/app/components/TableAgre.tsx
+++ src/app/components/TableAgre.tsx
@@ -13,13 +13,14 @@ interface Agreement {
 
 interface TableAgreementProps {
   agreements: Agreement[];
+  title: string; 
 }
 
-const TableAgreement: React.FC<TableAgreementProps> = ({ agreements }) => {
+const TableAgreement: React.FC<TableAgreementProps> = ({ agreements,title }) => {
   return (
     <div style={{ padding: '100px' }}>
       <div className="col text-center">
-        <h4 style={{ color: 'gray' }}>Acuerdos en Trámite</h4>
+      <h4 style={{ color: 'gray' }}>Acuerdos {title}</h4>
         <div className="scrollable-table-container">
           <table className="mi-tabla">
             <thead className="bg-light">
@@ -37,12 +38,12 @@ const TableAgreement: React.FC<TableAgreementProps> = ({ agreements }) => {
             <tbody>
               {agreements.map((agreement, index) => (
                 <tr key={index}>
-                  <td>{agreement.topic}</td>
+                  <td>{agreement.agreementId}</td>
                   <td>{agreement.sessionId}</td>
-                  <td>{agreement.description}</td>
+                  <td>{agreement.topic}</td>
                   <td>{agreement.asignedTo}</td>
-                  <td>{agreement.deadline}</td>
-                  <td>{agreement.deadline}</td>
+                  <td>{agreement.deadline.substring(0,10)}</td>
+                  <td>{agreement.deadline.substring(0,10)}</td>
                   <td>{agreement.state}</td>
                   <td>
                     <button className="btn btn-amarillo">Editar</button>
