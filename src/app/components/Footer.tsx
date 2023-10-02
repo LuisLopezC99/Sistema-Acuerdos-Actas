2fd069727fee6108f2fa9180eaf1a4304d361e75
Jose0696 <jose96seque@gmail.com>
2023-10-01 23:38:16 -0600

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,11 +5,11 @@ import { Container } from "react-bootstrap";
 const Footer = () => {
     return (
         <footer className="bg-white text-white fixed-bottom">
-            <Container>
+            
                 <div className="d-flex justify-content-between align-items-center py-0">
                     <p className="custom-footer-text">&copy; 2023 Sistema de Acuerdos y Actas Municipales. Todos los derechos reservados</p>
                 </div>
-            </Container>
+            
         </footer>
     );
 }
